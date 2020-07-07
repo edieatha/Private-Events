18a094239543938713da082ae5a187b5835aa72b
Joseph Mindo <mindo.joseph.mj@gmail.com>
2020-07-08 00:10:26 +0300

    Modify show page for user

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,11 @@
 Rails.application.routes.draw do
+  root 'events#index'
+  resources :events, only: %i[index new create show]
+  
   get 'users/new'
   get 'users/create'
   get 'users/show'
   devise_for :users
-  root 'home#index'
+  
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
