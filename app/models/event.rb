e9a8631ddc080552622acff5a869b6af5fbd5ef8
Edie Atha <edieatha@gmail.com>
2020-07-10 15:50:27 +0300

    1. Updated Events table. 2. New Invitation model for through table. 3. Updated events model

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,7 +1,7 @@
 class Event < ApplicationRecord
-  belongs_to :creator, foreign_key: 'creator_id', class_name: 'User'
-  has_many :attendees, through: :event_attendees, source: :event_attendee
-  has_many :event_attendees, foreign_key: 'attended_event_id'
+  belongs_to :creator, class_name: 'User'
+  has_many :attendees, through: :invitations, source: :attendee
+  has_many :invitations, foreign_key: 'attended_event_id'
 
   validates :description, presence: true
 end
