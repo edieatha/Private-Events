87d418cdaffd91762d043fb3b083b5147e168f69
edieatha <edieatha@gmail.com>
2020-07-14 20:59:12 +0300

    Stickler errors fixed

--- spec/models/event_spec.rb
+++ spec/models/event_spec.rb
@@ -2,9 +2,7 @@ require 'rails_helper'
 RSpec.describe Event, type: :model do
   context 'Validation for Event model'
   let(:user) { User.create(username: 'Testing') }
-  # rubocop:disable Layout/LineLength
   let(:event) { Event.new(title: 'Testing event', location: 'Testing_loc', description: 'des_test', date: '2020-06-16', creator_id: user.id) }
-  # rubocop:enable Layout/LineLength
   it 'Event should be valid if it has a title' do
     expect(event).to be_valid
   end
