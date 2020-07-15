6458d0e77d814b4b4885c9d612873cdca300ccde
edieatha <edieatha@gmail.com>
2020-07-15 19:04:52 +0300

    spec/models/user_spec.rb

--- spec/models/event_spec.rb
+++ spec/models/event_spec.rb
@@ -2,7 +2,7 @@ require 'rails_helper'
 
 RSpec.describe Event, type: :model do
   context 'Validation for Event model'
-  let(:user) { User.create(email: 'test@test.com', password: 'testing') }
+  let(:user) { User.new(email: 'user@test.com', password: 'testing') }
   let(:event) { Event.new(even_name: 'event name', location: 'location', description: 'description', date: '19-04-2020', creator_id: user.id) }
 
   it 'VALID if with event name' do
