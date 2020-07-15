5d112af372468104f4d323e35bba50c09d9d698f
Joseph Mindo <mindo.joseph.mj@gmail.com>
2020-07-15 17:56:14 +0300

    remove double quote string

--- config/spring.rb
+++ config/spring.rb
@@ -1,12 +1,6 @@
 Spring.watch(
-<<<<<<< HEAD
-  ".ruby-version",
-  ".rbenv-vars",
-  "tmp/restart.txt",
-=======
   '.ruby-version',
   '.rbenv-vars',
   'tmp/restart.txt',
->>>>>>> 48187f7963bc806ec38c20b1c1bea59403baf111
   'tmp/caching-dev.txt'
 )
