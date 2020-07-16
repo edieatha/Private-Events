01e98c96667e298843661c8b1e72fb02891174c1
Joseph Mindo <mindo.joseph.mj@gmail.com>
2020-07-16 18:15:48 +0300

    Remove logic from views

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,5 +2,8 @@ class UsersController < ApplicationController
   def show
     @user = User.find(params[:id])
     @invitations = @user.invitations
+    @created_events = current_user.events
+    @attending_events = current_user.attended_events
+    @attended_events = Event.past_events
   end
 end
