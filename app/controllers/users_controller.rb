544afaf2af4a25ea16bf3705e274613e11e08af9
edieatha <edieatha@gmail.com>
2020-07-17 17:24:53 +0300

    Fixed nomethod error in users index

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,6 +4,10 @@ class UsersController < ApplicationController
     @invitations = @user.invitations
     @attended_events = current_user.attended_events
     @past_events_attended = Event.past_events
-    @created_events = current_user.events
+  end
+
+  def index
+    @created_events = current_user.events    
+    @attended_events = current_user.attended_events
   end
 end
