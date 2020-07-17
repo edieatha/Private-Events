74f117d10de6840ddbc0d56664e245079a09fb68
edieatha <edieatha@gmail.com>
2020-07-18 01:06:47 +0300

    Linter error fixed

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -7,7 +7,7 @@ class UsersController < ApplicationController
   end
 
   def index
-    @created_events = current_user.events    
+    @created_events = current_user.events
     @attended_events = current_user.attended_events
   end
 end
